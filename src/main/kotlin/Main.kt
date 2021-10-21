4b5cf127eb16c48ce064919b087f30eece270a19
aymn <aymn.ali.ruh.k@tuwaiq.edu.sa>
2021-10-21 11:27:13 +0300

    home work fix bugs and new Student ,professor and cours

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,45 +1,56 @@
-fun main(args: Array<String>) {
+    fun main(args: Array<String>) {
 var st1=Student("aymn","ali",2)
 var st2=Student("rakan","aaa",10)
-    var st3 =Student("khalid","aymn",5)
-    var st6 =Student("dafer","halo",6)
-    var st4=Student("x","c",5)
-    var st5=Student("tt","joa",56)
-    var prof1=professor("ali","khalid","055412365","math")
-var cours1=cours("math",50,5,4)
-    cours1.professor=prof1.FullPFN()
-    println(cours1.IfAssint()).toString()
-    println(cours1.CoursInformiton()).toString()
-
-}
-class cours(var coursName:String,var numOfLac:Int,var student:Int, var numStudent:Int) {
-    var professor:String=""
-    var Assient:Boolean = professor==""
-    fun IfAssint():String{
-        if (Assient) {
-            println("you can't asssient becose there is a professor")
-        }else{
-            println("joiend")}
-
-    return (professor)
+var st3 =Student("khalid","aymn",5)
+var st6 =Student("dafer","halo",6)
+var st4=Student("x","c",5)
+var st5=Student("tt","joa",56)
+var prof1=professor("ali","khalid","055412365","math")
+var prof2=professor("aymnAli","kedri","0112546","pa")
+var prof3=professor("khlead","al-qhtane","0112547","pc")
+var cours1=cours("math",50,5,40)
+var cours2=cours("pa",10,100,150)
+var cours3=cours("pc",15,149,150)
+        cours1.professor=prof1.FullPFN()
+        println(cours1.IfAssint()).toString()
+        println(cours1.CoursInformiton()).toString()
+        cours2.professor=prof2.FullPFN()
+        println(cours2.IfAssint()).toString()
+        println(cours2.CoursInformiton()).toString()
+        cours3.professor=prof3.FullPFN()
+        println(cours3.IfAssint()).toString()
+        println(cours3.CoursInformiton()).toString()
     }
-    var coursinfo: Boolean = student >= 5
-    fun CoursInformiton():String{
-        if (coursinfo) {
-            println("student can't joined becose it's full")
-        } else {
-            println("student joined the cours")
+    class  professor(var PFN:String,var PLN:String,var Tele:String,var coursess:String){
+        fun FullPFN():String{
+            return "$PFN $PLN"
         }
-    return ("$coursName $numOfLac $professor $numStudent")
+    }
+    class Student(FN:String,LN:String,courses:Int){
+
     }
 
+    class cours(var coursName:String,var numOfLac:Int,var student:Int, var numStudent:Int) {
+        var professor:String=""
+        var Assient:Boolean = professor==""
+        fun IfAssint():String{
+            if (Assient) {
+                println("you can't asssient becose there is a professor")
+            }else{
+                println("joiend")}
 
-}
-class  professor(var PFN:String,var PLN:String,var Tele:String,var coursess:String){
-    fun FullPFN():String{
-        return "$PFN $PLN"
+        return (professor)
+        }
+        var coursinfo: Boolean = student >= numStudent
+        fun CoursInformiton():String{
+            if (coursinfo) {
+                println("student can't joined becose it's full")
+            } else {
+                println("student joined the cours")
+            }
+        return ("$coursName $numOfLac $professor $numStudent")
+        }
+
+
     }
-}
-class Student(FN:String,LN:String,courses:Int){
 
-}
